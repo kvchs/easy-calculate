7952eafbbaf9e03db1ef6c8ee86bd2d1f497a8d7
Liuhaiyan6 <2391348695@qq.com>
2016-05-26 23:11:30 +0800

    commit

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,5 +1,21 @@
 function calculate_median(arr) {
-  //请勿改动函数名
+    var arr1=[];
+
+    for(var i =0;i <arr.length;i++){
+        if (i%2 == 1) {
+            arr1.push(arr[i]);
+
+        }
+
+    }
+    arr1.sort(function(a,b){return a-b});
+
+    var len = arr1.length;
+    if(len%2 == 0){
+        return (arr1[len/2] + arr1[len/2 - 1])/2;
+    }else{
+        return arr1[(len+1)/2-1];
+    }
 }
 
 module.exports = calculate_median;
